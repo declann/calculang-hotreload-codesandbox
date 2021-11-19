e9ee54b03b21bde5e095ae8bcb1edc455b2d5d67
Declan Naughton <declan.naughton2@mail.dcu.ie>
2021-11-19 09:59:07 +0000

    temp-loader can work?

---
+++ model/temp-loader.js
@@ -0,0 +1,8 @@
+/*
+
+This could do the introspection api call for the entrypoint (only)
+and pass the result to options for calculang-transform-loader
+
+I don't need HMR in the model compilation (no big state concerns), moreso the app itself.
+
+*/
